87d2164c7cb21e8df3d2caa6565df3c45b6a213f
mohammadsyed397 <mohammadsyed601@gmail.com>
2024-07-26 17:01:12 -0400

    my first commit to intiger branch

---
+++ inti.py
@@ -0,0 +1,15 @@
+#This program contains various operations performed using intigers
+num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(num2-num1)
+print("Absolute Value:", result3)
