8d6b46b96d5539c272dab4a74096d5f401712f7b
mohammadsyed397 <mohammadsyed601@gmail.com>
2024-08-08 19:45:00 -0400

    my second commit

--- Commandlineargs.py
+++ Commandlineargs.py
@@ -10,12 +10,15 @@ def mul(num1, num2):
     mul = num1*num2
     return mul
 def division(num1, num2):
-    if num2 == 0:
-       print("Invalid input",)
-       
-    else:
      division = num1/num2
      return division
+def floatdivision(num1, num2):
+    floatdivision = num1//num2
+    return floatdivision
+def reminder(num1, num2):
+    reminder = num1%num2
+    return reminder
+    
 num1 = int(sys.argv[1])
 operation = sys.argv[2]
 num2 = int(sys.argv[3])
@@ -31,5 +34,11 @@ if operation == "*":
 if operation == "/":
    output = division(num1, num2)
    print(output)
+if operation == "//":
+   output = floatdivision(num1, num2)
+   print(output)
+if operation == "%":
+    output = reminder(num1, num2)
+    print(output)
 
 
